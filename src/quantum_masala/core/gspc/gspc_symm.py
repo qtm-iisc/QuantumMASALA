13ec9f7dcc00f1e1e1fecdc3f1b106950dda5372
lightpeak008 <35771613+lightpeak008@users.noreply.github.com>
2022-12-11 12:27:13 +0530

    Optimized charge symmetrization routines

--- src/quantum_masala/core/gspc/gspc_symm.py
+++ src/quantum_masala/core/gspc/gspc_symm.py
@@ -6,11 +6,17 @@ from quantum_masala.core import Crystal
 from quantum_masala.constants import TPIJ
 from quantum_masala.core.spglib_utils import get_symmetry_crystal
 
+ROUND_PREC: int = 6
+
 
 class SymmMod:
-    def __init__(self, cryst: Crystal, gspc_cryst: np.ndarray):
-        reallat_symm, recilat_symm = get_symmetry_crystal(cryst)
+    def __init__(self, crystal: Crystal, g_cryst: np.ndarray):
+        recilat = crystal.recilat
+        self.numg = g_cryst.shape[1]
+        g_norm2 = recilat.norm2(g_cryst)
+        idxsort = np.lexsort((*g_cryst, np.around(g_norm2, ROUND_PREC)))
 
+        reallat_symm, recilat_symm = get_symmetry_crystal(crystal)
         if reallat_symm is not None:
             self.numsymm = len(reallat_symm)
             rot_recispc = recilat_symm
@@ -20,62 +26,35 @@ class SymmMod:
             rot_recispc = np.eye(3).reshape(1, 3, 3)
             trans_realspc = np.zeros((1, 3))
 
-        ix, iy, iz = gspc_cryst
-        self.gspc_size = gspc_cryst.shape[1]
-        ax, ay, az = np.amin(ix), np.amin(iy), np.amin(iz)
-        bx, by, bz = np.amax(ix) - ax + 1, np.amax(iy) - ay + 1, np.amax(iz) - az + 1
+        l_grot_rank = np.zeros((self.numsymm, self.numg), dtype='i8')
+        for isymm in range(self.numsymm):
+            grot_cryst = rot_recispc[isymm] @ g_cryst
+            grot_norm2 = np.around(recilat.norm2(grot_cryst), ROUND_PREC)
+            isort = np.lexsort((*grot_cryst, grot_norm2))
+            l_grot_rank[isymm][isort] = np.arange(self.numg, dtype='i8')
 
-        rank = (ix - ax) + bx * (iy - ay) + bx * by * (iz - az) + 1
+        g_rank = np.amin(l_grot_rank, axis=0)
+        _, l_istar = np.unique(g_rank, return_index=True)
+        shell_idx = l_grot_rank.T[l_istar]
+        shell_idx = idxsort[shell_idx]
+        isort = np.argsort(shell_idx[:, 0])
+        shell_idx = np.array(shell_idx[isort])
 
-        rank2idx = np.ones(bx * by * bz, dtype="i8") * -1
-        rank2idx[rank] = np.arange(self.gspc_size)
+        l_grot_shell = np.tensordot(rot_recispc, g_cryst[:, shell_idx[:, 0]], axes=1)
+        shell_phase = np.exp(TPIJ * np.sum(l_grot_shell * trans_realspc.reshape((-1, 3, 1)), axis=1))
+        shell_phase = np.array(shell_phase.T)
+        g_phase = np.zeros(self.numg, dtype='c16')
+        g_count = np.zeros(self.numg, dtype='i8')
+        for isymm in range(self.numsymm):
+            g_phase[shell_idx[:, isymm]] += shell_phase[:, isymm]
+            g_count[shell_idx[:, isymm]] += 1
 
-        l_cryst_rot = np.tensordot(rot_recispc, gspc_cryst, axes=1)
-        l_phase_rot = np.exp(
-            TPIJ
-            * np.sum(l_cryst_rot * trans_realspc.reshape((self.numsymm, 3, 1)), axis=1)
-        )
-        l_phase_rot = np.ascontiguousarray(l_phase_rot.T)
-
-        l_rank_rot = np.rint(
-            (l_cryst_rot[:, 0] - ax)
-            + bx * (l_cryst_rot[:, 1] - ay)
-            + bx * by * (l_cryst_rot[:, 2] - az)
-            + 1
-        ).astype("i8")
-
-        l_rank_rot = np.ascontiguousarray(l_rank_rot.T)
-
-        g_done = np.zeros(self.gspc_size, dtype=bool)
-        g_phase = np.zeros(self.gspc_size, dtype="c16")
-        g_count = np.zeros(self.gspc_size, dtype="i8")
-
-        shell_phase = []
-        shell_idx = []
-
-        for ig in range(self.gspc_size):
-            if g_done[ig]:
-                continue
-
-            g_rank_rot = l_rank_rot[ig]
-            g_phase_rot = l_phase_rot[ig]
-            ig_rot = rank2idx[g_rank_rot]
-
-            for isymm in range(self.numsymm):
-                jg = ig_rot[isymm]
-                g_phase[jg] += g_phase_rot[isymm]
-                g_count[jg] += 1
-
-            shell_idx.append(ig_rot)
-            shell_phase.append(g_phase_rot)
-            g_done[ig_rot] = True
-
-        self.shell_phase = np.array(shell_phase)
+        self.shell_idx = shell_idx
+        self.shell_phase = shell_phase
         self.g_phaseconj = np.conj(g_phase) / g_count
-        self.shell_idx = np.array(shell_idx)
 
     def _symmetrize(self, arr_g: np.ndarray):
-        shell_avg = np.empty(self.gspc_size, dtype="c16")
+        shell_avg = np.empty(self.numg, dtype="c16")
         shell_idx_ = (Ellipsis, self.shell_idx)
         shell_avg[shell_idx_] = (
             np.sum(arr_g[shell_idx_] * self.shell_phase, axis=-1, keepdims=True)
@@ -86,11 +65,11 @@ class SymmMod:
         return shell_avg
 
     def symmetrize(self, arr_g: np.ndarray):
-        if arr_g.shape[-1] != self.gspc_size:
+        if arr_g.shape[-1] != self.numg:
             raise ValueError(
-                f"'arr_g.shape' invalid. Expected {(..., self.gspc_size)}, got {arr_g.shape}"
+                f"'arr_g.shape' invalid. Expected {(..., self.numg)}, got {arr_g.shape}"
             )
 
-        arr_ = arr_g.reshape(-1, self.gspc_size)
+        arr_ = arr_g.reshape(-1, self.numg)
         for i in range(arr_.shape[0]):
             arr_[i] = self._symmetrize(arr_[i])
