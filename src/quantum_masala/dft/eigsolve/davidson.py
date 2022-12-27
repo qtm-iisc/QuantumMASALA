f24e0ea215b89141924ed3a76a0f16afce2bf788
lightpeak008 <35771613+lightpeak008@users.noreply.github.com>
2022-12-27 22:14:27 +0530

    Fixed bug in davidson diagonalization + added calls to timers

--- src/quantum_masala/dft/eigsolve/davidson.py
+++ src/quantum_masala/dft/eigsolve/davidson.py
@@ -3,13 +3,14 @@ __all__ = ['solver']
 from time import perf_counter
 import numpy as np
 
-from quantum_masala import config
+from quantum_masala import config, pw_counter
 from quantum_masala.dft.ksham import KSHam
 
 
 def solver(ham: KSHam,
            diago_thr: float, evc_gk: np.ndarray,
            vbare_g0: float):
+    pw_counter.start_clock('david')
     # Choosing backend
     if config.use_gpu:
         import cupy as xp
@@ -22,11 +23,13 @@ def solver(ham: KSHam,
                  * (ham.l_vkb_H.T * ham.l_vkb), axis=0)
 
     def g_psi(psi_: xp.ndarray, l_evl_: xp.ndarray):
+        pw_counter.start_clock('david:g_psi')
         nonlocal ham_diag
         scala = 2
         x = scala * (ham_diag.reshape(1, -1) - l_evl_.reshape(-1, 1))
         denom = 0.5 * (1 + x + np.sqrt(1 + (x - 1) * (x - 1))) / scala
         psi_ /= denom
+        pw_counter.stop_clock('david:g_psi')
 
     # Intra k-group Communicator for band parallelization
     kgrp_intracomm = config.pwcomm.kgrp_intracomm
@@ -46,32 +49,29 @@ def solver(ham: KSHam,
     ndim_max = config.davidson_numwork * numeig
     psi = xp.empty((ndim_max, numgk), dtype=xp.complex128)
     hpsi = xp.empty((ndim_max, numgk), dtype=xp.complex128)
-    ham_red = xp.empty((ndim_max, ndim_max), dtype=xp.complex128)
-    ovl_red = xp.empty((ndim_max, ndim_max), dtype=xp.complex128)
+    ham_red = xp.zeros((ndim_max, ndim_max), dtype=xp.complex128)
+    ovl_red = xp.zeros((ndim_max, ndim_max), dtype=xp.complex128)
     evc_red = xp.empty((numeig, ndim_max), dtype=xp.complex128, order='F')
     evl_red = xp.empty(numeig, dtype=xp.complex128)
 
     ndim = numeig
     psi[:ndim] = evc
 
-    numhpsi = 0
-    hpsi_time = 0
     def compute_hpsi(istart: int, istop: int):
-        nonlocal numhpsi, hpsi_time
-        start_time = perf_counter()
-        numhpsi += istop - istart
+        pw_counter.start_clock('david:h_psi')
         sl = kgrp_intracomm.psi_scatter_slice(istart, istop)
         if sl.stop > sl.start:
             ham.h_psi(psi[sl], hpsi[sl])
         kgrp_intracomm.barrier()
         kgrp_intracomm.psi_Allgather_inplace(hpsi[istart:istop])
-        hpsi_time += perf_counter() - start_time
+        pw_counter.stop_clock('david:h_psi')
 
     def move_unconv():
         evc_red[:nunconv] = evc_red[:numeig][unconv_flag]
         evl_red[:nunconv] = evl_red[:numeig][unconv_flag]
 
     def expand_psi():
+        pw_counter.start_clock('david:update')
         nonlocal ndim
         sl = slice(ndim, ndim + nunconv)
         sl_bgrp = kgrp_intracomm.psi_scatter_slice(0, ndim)
@@ -86,18 +86,20 @@ def solver(ham: KSHam,
         psi[sl] /= xp.linalg.norm(psi[sl], axis=1, keepdims=True)
         kgrp_intracomm.barrier()
         ndim += nunconv
+        pw_counter.stop_clock('david:update')
 
     def solve_red():
         ham_red_ = ham_red[:ndim, :ndim]
         ovl_red_ = ovl_red[:ndim, :ndim]
         evc_red_ = evc_red[:, :ndim].T
 
-        sl_bgrp = kgrp_intracomm.psi_scatter_slice(0, ndim)
+        sl_bgrp = kgrp_intracomm.psi_scatter_slice(ndim-nunconv, ndim)
         ham_red_[sl_bgrp] = psi[sl_bgrp].conj() @ hpsi[:ndim].T
         ovl_red_[sl_bgrp] = psi[sl_bgrp].conj() @ psi[:ndim].T
         kgrp_intracomm.psi_Allgather_inplace(ham_red[0:ndim])
         kgrp_intracomm.psi_Allgather_inplace(ovl_red[0:ndim])
 
+        pw_counter.start_clock('david:eigh')
         if kgrp_intracomm.rank == 0:
             if config.use_gpu:
                 # Generalized Eigenvalue Problem Ax = eBx
@@ -111,11 +113,12 @@ def solver(ham: KSHam,
                 evl_red[:numeig] = A_evl[:numeig]
                 evc_red[:, :ndim] = xp.linalg.solve(L.conj().T, A_evc[:, :numeig]).T
             else:
-                evl_red[:], evc_red_[:] = eigh(ham_red_, ovl_red_,
-                                               subset_by_index=[0, numeig-1])
-
+                evl_red[:], evc_red_[:] = eigh(
+                    ham_red_, ovl_red_, subset_by_index=[0, numeig-1],
+                )
         kgrp_intracomm.Bcast(evl_red)
         kgrp_intracomm.Bcast(evc_red[:, :ndim])
+        pw_counter.stop_clock('david:eigh')
 
     compute_hpsi(0, ndim)
     solve_red()
@@ -145,12 +148,12 @@ def solver(ham: KSHam,
 
             ndim = numeig
             ham_red[:], ovl_red[:], evc_red[:, :] = 0, 0, 0
-            ham_red[(range(numeig), range(numeig))] = evl
-            ovl_red[(range(numeig), range(numeig))] = 1
-            evc_red[(range(numeig), range(numeig))] = 1
+            xp.fill_diagonal(ham_red, evl)
+            xp.fill_diagonal(ovl_red, 1)
+            xp.fill_diagonal(evc_red, 1)
 
-    stats = np.array([idxiter, numhpsi, hpsi_time])
+    pw_counter.stop_clock('david')
     if config.use_gpu:
-        return xp.asnumpy(evl.real), xp.asnumpy(evc), stats
+        return xp.asnumpy(evl.real), xp.asnumpy(evc), idxiter
     else:
-        return evl.real, evc, stats
+        return evl.real, evc, idxiter
