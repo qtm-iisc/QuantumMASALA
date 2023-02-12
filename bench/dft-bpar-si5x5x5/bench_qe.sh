006e7028e09c9b3f96d93f4dff9896dfe91096e9
lightpeak008 <35771613+lightpeak008@users.noreply.github.com>
2023-02-12 12:12:29 +0530

    Modified benchmark script

--- bench/dft-bpar-si5x5x5/bench_qe.sh
+++ bench/dft-bpar-si5x5x5/bench_qe.sh
@@ -2,7 +2,16 @@
 
 export OMP_NUM_THREADS=2
 echo "Setting OMP_NUM_THREADS to $OMP_NUM_THREADS"
-for pow in $(seq 1 1 5); do
+for pow in $(seq 0 1 4); do
+    nproc=$((2 ** pow))
+
+    echo "Running QE in $nproc processes"
+    mpirun -np $nproc pw.x -nk $nproc -in si5x5x5.scf.in > $nproc.qe.out
+done
+
+export OMP_NUM_THREADS=1
+echo "Setting OMP_NUM_THREADS to $OMP_NUM_THREADS"
+for pow in $(seq 0 1 4); do
     nproc=$((2 ** pow))
 
     echo "Running QE in $nproc processes"
--- bench/dft-bpar-si5x5x5/bench_qtm.sh
+++ bench/dft-bpar-si5x5x5/bench_qtm.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-export OMP_NUM_THREADS=2
+echo "Running QTM in GPU"
+python si5x5x5_scf.py --use-gpu > gpu.py.out
+
+export OMP_NUM_THREADS=1
 echo "Setting OMP_NUM_THREADS to $OMP_NUM_THREADS"
-for pow in $(seq 1 1 5); do
+for pow in $(seq 0 1 4); do
     nproc=$((2 ** pow))
 
     echo "Running QTM in $nproc processes"
-    mpirun -np $nproc python -u si5x5x5_scf.py > $nproc.py.out
+    mpirun -np $nproc python -u si5x5x5_scf.py -nk 1 > $nproc.py.out
 done
 
-echo "Running QTM in GPU"
-python si5x5x5_scf.py --use-gpu > gpu.py.out
+export OMP_NUM_THREADS=2
+echo "Setting OMP_NUM_THREADS to $OMP_NUM_THREADS"
+for pow in $(seq 0 1 4); do
+    nproc=$((2 ** pow))
+
+    echo "Running QTM in $nproc processes"
+    mpirun -np $nproc python -u si5x5x5_scf.py -nk 1> $nproc.py.out
+done
