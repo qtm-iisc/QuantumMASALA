af6ab0833ac59827dd08b0ee5a6761343e64ce0e
lightpeak008 <35771613+lightpeak008@users.noreply.github.com>
2023-08-19 20:37:23 +0530

    Added docstrings for wavefun.py

--- src/qtm/containers/wavefun.py
+++ src/qtm/containers/wavefun.py
@@ -1,4 +1,7 @@
 from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from typing import Literal
 __all__ = ['Wavefun', 'WavefunG', 'WavefunR',
            'WavefunSpinG', 'WavefunSpinR']
 
@@ -28,10 +31,7 @@ class Wavefun(Buffer, ABC):
 
     @abstractmethod
     def __new__(cls, gkspc: GkSpace, data: NDArray):
-        if cls.numspin is None:
-            raise NotImplementedError(
-                f"{Wavefun} subclass did not set class attribute 'numspin'."
-            )
+        assert cls.numspin is not None
         return Buffer.__new__(cls, gkspc, data)
 
     def __init__(self, gkspc: GkSpace, data: NDArray):
@@ -58,27 +58,28 @@ class WavefunG(Wavefun):
         return Wavefun.__new__(cls, gkspc, data)
 
     @classmethod
-    def _get_basis_size(cls, gkspc: GkSpace):
+    def _get_basis_size(cls, gkspc: GkSpace) -> int:
         return cls.numspin * gkspc.size_g
 
     def __init__(self, gkspc: GkSpace, data: NDArray):
         Wavefun.__init__(self, gkspc, data)
 
     @property
-    def basis_type(self):
+    def basis_type(self) -> Literal['g']:
         return 'g'
 
-    def to_g(self):
+    def to_g(self) -> WavefunG:
         return self
 
-    def to_r(self):
+    def to_r(self) -> WavefunR:
         gkspc = self.gkspc
         data_g = self.data.reshape((*self.shape, self.numspin, -1))
 
         data_r = gkspc.g2r(data_g).reshape((*self.shape, -1))
         return WavefunR(gkspc, data_r)
 
-    def norm2(self):
+    def norm2(self) -> NDArray:
+        """Returns the norm-squared value of the wavefunction vectors."""
         vdot = self.gspc.allocate_array(self.shape)
         for iwfn, wfn in enumerate(self.data.reshape((-1, self.basis_size))):
             vdot.ravel()[iwfn] = np.vdot(wfn, wfn)
@@ -88,13 +89,30 @@ class WavefunG(Wavefun):
             comm.Allreduce(comm.IN_PLACE, vdot, comm.SUM)
         return vdot
 
-    def norm(self):
+    def norm(self) -> NDArray:
+        """Returns the norm of the wavefunction vectors."""
         return np.sqrt(self.norm2())
 
     def normalize(self):
+        """Normalizes the wavefunction so that its norm is 1."""
         self.data[:] /= self.norm()[:, np.newaxis]
 
-    def vdot(self, ket: WavefunG):
+    def vdot(self, ket: WavefunG) -> NDArray:
+        """Evaluates the vector dot product between the wavefunctions in two
+        `WavefunG` instances. The values of the instance are conjugated, not
+        the ones in the method input.
+
+        Parameters
+        ----------
+        ket : WavefunG
+            Wavefunction to evaluate the dot product with
+
+        Returns
+        -------
+        NDArray
+            Array containing the vector dot product between the wavefunctions
+            in instance and the ones in the input argument.
+        """
         # TODO: Reprofile this and optimize it for memory usage
         # TODO: Reevaluate this implementation when SciPy is compiled with CBLAS too
         # TODO: Add check for C-Contiguity
@@ -134,17 +152,17 @@ class WavefunR(Wavefun):
         return Wavefun.__new__(cls, gkspc, data)
 
     @classmethod
-    def _get_basis_size(cls, gkspc: GkSpace):
+    def _get_basis_size(cls, gkspc: GkSpace) -> int:
         return cls.numspin * gkspc.size_r
 
     @property
-    def basis_type(self):
+    def basis_type(self) -> Literal['r']:
         return 'r'
 
-    def to_r(self):
+    def to_r(self) -> WavefunR:
         return self
 
-    def to_g(self):
+    def to_g(self) -> WavefunG:
         gkspc = self.gkspc
         data_r = self.data.reshape((*self.shape, self.numspin, -1))
 
@@ -152,6 +170,18 @@ class WavefunR(Wavefun):
         return WavefunG(gkspc, data_g)
 
     def get_density(self, normalize: bool = True) -> FieldR:
+        """"Constructs the probability density from the stored wavefunctions
+
+        Parameters
+        ----------
+        normalize : bool, default=True
+            If True, normalize the densities so that they integrate to one.
+
+        Returns
+        -------
+        FieldR
+            Represents the probability density of the wavefunctions.
+        """
         gwfn = self.gkspc.gwfn
         den_data = self.data.conj()
         den_data *= self.data
@@ -176,7 +206,7 @@ class WavefunSpinG(WavefunG):
             return DistWavefun.__new__(DistWavefunSpinG, gkspc, data)
         return Wavefun.__new__(cls, gkspc, data)
 
-    def to_r(self):
+    def to_r(self) -> WavefunSpinR:
         gkspc = self.gkspc
         data_g = self.data.reshape((*self.shape, self.numspin, -1))
 
@@ -196,7 +226,7 @@ class WavefunSpinR(WavefunR):
             return DistWavefun.__new__(DistWavefunR, gkspc, data)
         return Wavefun.__new__(cls, gkspc, data)
 
-    def to_g(self):
+    def to_g(self) -> WavefunSpinG:
         gkspc = self.gkspc
         data_r = self.data.reshape((*self.shape, self.numspin, -1))
 
