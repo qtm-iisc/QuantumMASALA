825537d69831dd5e216766d87128190a8e5f4221
lightpeak008 <35771613+lightpeak008@users.noreply.github.com>
2023-09-11 18:09:19 +0530

    Updated 'fixed.py'

--- src/qtm/dft/occup/fixed.py
+++ src/qtm/dft/occup/fixed.py
@@ -8,27 +8,33 @@ from qtm.dft.config import DFTCommMod
 from .utils import check_args
 
 
-def compute_occ(dftcomm: DFTCommMod, l_kswfn: list[KSWfn], numel: int):
-    check_args(dftcomm, l_kswfn, numel)
+def compute_occ(dftcomm: DFTCommMod, l_kswfn: list[list[KSWfn]], numel: int):
+    with dftcomm.image_comm as comm:
+        assert isinstance(numel, int)
+        assert numel == comm.bcast(numel)
+        assert numel % 2 == 0
+        assert all(len(kswfn_k) == 1 for kswfn_k in l_kswfn)
 
     with dftcomm.image_comm as image_comm:
         numel = image_comm.bcast(numel)
-        assert isinstance(numel, int) and numel % 2 == 0
-
-        numbnd = l_kswfn[0].numbnd
+        numbnd = l_kswfn[0][0].numbnd
         numfill = numel // 2
-        for wfn, in l_kswfn:
-            wfn.occ[:, :numfill] = 1
-            wfn.occ[:, numfill:] = 0
+        for wfn_k in l_kswfn:
+            assert len(wfn_k) == 1
+            wfn_k[0].occ[:numfill] = 1
+            wfn_k[0].occ[numfill:] = 0
 
         max_filled, min_empty = None, None
         with dftcomm.kroot_intra as comm:
-            max_filled = comm.allreduce(float(
-                max(np.amax(wfn.evl[:, numfill]) for wfn in l_kswfn)
+            if comm.is_null:
+                comm.skip_with_block()
+
+            max_filled = comm.allreduce(max(
+                np.amax(wfn_k[0].evl[:numfill]) for wfn_k in l_kswfn
             ), comm.MAX)
             if numfill < numbnd:
-                min_empty = comm.allreduce(float(
-                    min(np.amin(wfn.evl[numfill:]) for wfn in l_kswfn)
+                min_empty = comm.allreduce(min(
+                    np.amin(wfn_k[0].evl[numfill:]) for wfn_k in l_kswfn
                 ), comm.MIN)
         max_filled = comm.bcast(max_filled)
         min_empty = comm.bcast(min_empty)
